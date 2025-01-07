161013233c76b9210a9f175be84e54fae7b32342
Harsh Kumar <hadron43@yahoo.com>
2025-01-07 22:38:45 +0530

    feat, add homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,48 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { FaKey } from "react-icons/fa";
+import Link from "next/link";
+import "./globals.css";
+import { TypeAnimation } from "react-type-animation";
+
+const Home = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="flex flex-col items-center justify-center min-h-screen text-center text-white bg-background">
+      <h1 className="text-4xl font-bold"></h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+      <TypeAnimation
+        sequence={[
+          "",
+          500,
+          "Welcome seeker of mystery and lore...",
+          1000,
+          "Through digital realms you'll explore...",
+          1500,
+          "Hidden wonders await your keen eye...",
+          1500,
+          "In places where cobwebs lie...",
+          1500,
+          "Fortune favors those who dare to peek...",
+          1500,
+          "Below the surface, what you seek...",
+          2000,
+        ]}
+        wrapper="span"
+        speed={50}
+        deletionSpeed={75}
+        style={{ fontSize: "2em", display: "inline-block" }}
+        repeat={Infinity}
+      />
+      <footer className="absolute bottom-4 left-4">
+        <Link href="/secrets">
+          <FaKey
+            className="opacity-10 transition-all duration-300 hover:text-yellow-400 hover:scale-125 hover:opacity-100"
+            size={12}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        </Link>
       </footer>
     </div>
   );
-}
+};
+
+export default Home;
---
+++ src/app/secrets.tsx
@@ -0,0 +1,9 @@
+import "./globals.css";
+
+export default function Secrets() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen text-center text-white bg-background">
+      <h1 className="text-4xl font-bold">This is the secrets page!</h1>
+    </div>
+  );
+}
